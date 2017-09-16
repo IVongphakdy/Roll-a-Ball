9d5c4e8a1d8aedc66025009cd1e49cdc306af88a
izk <izk@izks-MacBook-Pro.local>
2017-09-16 13:10:28 -0700

    First commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI(){
+
+		if (GUI.Button (new Rect (10, 10, 100, 45), "Roll A Ball")) {
+			Application.LoadLevel(1);
+		}
+		if (GUI.Button (new Rect (10, 65, 100, 45), "Boxes")) {
+			Application.LoadLevel(2);
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public Text countText;
+	public Text winText;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	private Vector3 spawn;
+	private int ammoCount;
+
+	// Use this for initialization 
+	void Start () { 
+		moveSpeed = 10f; 
+		spawn = transform.position;
+		winText.text = "";
+		SetCountText ();
+		ammoCount = 0;
+	} 
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(moveSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+	
+		if (transform.position.y < -2)
+			Die ();
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			if (ammoCount > 0) {
+				Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+				ammoCount--;
+				SetCountText ();
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.transform.tag == "Enemy") {
+			Die ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Goal")
+			winText.text = "You Win!";
+		
+		if (other.gameObject.CompareTag ("Ammo")){
+			ammoCount++;
+			other.gameObject.SetActive (false);
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Ammo: " + ammoCount.ToString ();
+	}
+
+	void OnGUI(){
+		if(GUI.Button (new Rect(10, 10, 45, 45), "Home"))
+			Application.LoadLevel(0);
+	}
+
+	public void Die()
+	{
+		//transform.position = spawn;
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
